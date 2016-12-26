8208d4c04350c3a49be3fa0ce4f9254731523e37
daniiltserin <daniilborowkow@yandex.ru>
2016-12-26 08:25:46 +0600

    Readeed sitepoint article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService],
 })
 export class AppComponent {
-  title = 'app works!';
+  newTodo: Todo = new Todo();
+
+  todos: Todo[] = this.getTodos();
+
+  /**
+   * Ask NG dependency systen to inject the dependency
+   * associted with the dependency injection token `TodoDataService`
+   * and assign it to the property called todoDataService.
+   */
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  /**
+   * in view used implemented methods
+   */
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  getTodos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
